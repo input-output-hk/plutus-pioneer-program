63476046a28eef525b9e32c24b2ea88fe8454ffe
Lars Brünjes <brunjlar@gmail.com>
2021-05-03 21:20:42 +0200

    added 'Signed'

--- code/week05/src/Week05/Signed.hs
+++ code/week05/src/Week05/Signed.hs
@@ -10,7 +10,7 @@
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
 
-module Week05.Free where
+module Week05.Signed where
 
 import           Control.Monad              hiding (fmap)
 import           Control.Monad.Freer.Extras as Extras
@@ -33,39 +33,43 @@ import           Text.Printf                (printf)
 import           Wallet.Emulator.Wallet
 
 {-# INLINABLE mkPolicy #-}
-mkPolicy :: ScriptContext -> Bool
-mkPolicy _ = True
+mkPolicy :: PubKeyHash -> ScriptContext -> Bool
+mkPolicy pkh ctx = txSignedBy (scriptContextTxInfo ctx) pkh
 
-policy :: Scripts.MonetaryPolicy
-policy = mkMonetaryPolicyScript $$(PlutusTx.compile [|| Scripts.wrapMonetaryPolicy mkPolicy ||])
+policy :: PubKeyHash -> Scripts.MonetaryPolicy
+policy pkh = mkMonetaryPolicyScript $
+    $$(PlutusTx.compile [|| Scripts.wrapMonetaryPolicy . mkPolicy ||])
+        `PlutusTx.applyCode`
+            PlutusTx.liftCode pkh
 
-curSymbol :: CurrencySymbol
-curSymbol = scriptCurrencySymbol policy
+curSymbol :: PubKeyHash -> CurrencySymbol
+curSymbol = scriptCurrencySymbol . policy
 
 data MintParams = MintParams
     { mpTokenName :: !TokenName
     , mpAmount    :: !Integer
     } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
-type FreeSchema =
+type SignedSchema =
     BlockchainActions
         .\/ Endpoint "mint" MintParams
 
 mint :: (HasBlockchainActions s, AsContractError e) => MintParams -> Contract w s e ()
 mint mp = do
-    let val     = Value.singleton curSymbol (mpTokenName mp) (mpAmount mp)
-        lookups = Constraints.monetaryPolicy policy
+    pkh <- pubKeyHash <$> Contract.ownPubKey
+    let val     = Value.singleton (curSymbol pkh) (mpTokenName mp) (mpAmount mp)
+        lookups = Constraints.monetaryPolicy $ policy pkh
         tx      = Constraints.mustForgeValue val
     ledgerTx <- submitTxConstraintsWith @Void lookups tx
     void $ awaitTxConfirmed $ txId ledgerTx
     Contract.logInfo @String $ printf "forged %s" (show val)
 
-endpoints :: Contract () FreeSchema Text ()
+endpoints :: Contract () SignedSchema Text ()
 endpoints = mint' >> endpoints
   where
     mint' = endpoint @"mint" >>= mint
 
-mkSchemaDefinitions ''FreeSchema
+mkSchemaDefinitions ''SignedSchema
 
 mkKnownCurrencies []
 
