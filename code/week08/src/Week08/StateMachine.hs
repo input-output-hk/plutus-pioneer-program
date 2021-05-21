b1eed7b35803eb91acf9139ccd4057f83267b6df
Lars Brünjes <brunjlar@gmail.com>
2021-05-22 01:37:05 +0200

    started with week 8

---
+++ code/week08/src/Week08/StateMachine.hs
@@ -0,0 +1,204 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module Week08.StateMachine where
+
+import           Control.Monad                hiding (fmap)
+import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Text                    (Text, pack)
+import           GHC.Generics                 (Generic)
+import           Plutus.Contract              as Contract hiding (when)
+import           Plutus.Contract.StateMachine
+import           Plutus.Contract.Test
+import qualified PlutusTx
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
+import           Ledger                       hiding (singleton)
+import           Ledger.Ada                   as Ada
+import           Ledger.Constraints           as Constraints
+import           Ledger.Typed.Tx
+import qualified Ledger.Typed.Scripts         as Scripts
+import           Ledger.Value
+import           Playground.Contract          (ToSchema)
+import           Prelude                      (Semigroup (..))
+import qualified Prelude
+
+data TokenSale = TokenSale
+    { tsSeller :: !PubKeyHash
+    , tsToken  :: !AssetClass
+    , tsNFT    :: !AssetClass
+    } deriving (Show, Generic, FromJSON, ToJSON, Prelude.Eq, Prelude.Ord)
+
+PlutusTx.makeLift ''TokenSale
+
+data TSRedeemer =
+      SetPrice Integer
+    | AddTokens Integer
+    | BuyTokens Integer
+    | Withdraw Integer Integer
+    deriving Show
+
+PlutusTx.unstableMakeIsData ''TSRedeemer
+
+{-# INLINABLE lovelaces #-}
+lovelaces :: Value -> Integer
+lovelaces = Ada.getLovelace . Ada.fromValue
+
+{-# INLINABLE transition #-}
+transition :: TokenSale -> State Integer -> TSRedeemer -> Maybe (TxConstraints Void Void, State Integer)
+transition ts s r = case (stateValue s, stateData s, r) of
+    (v, _, SetPrice p)   -> Just ( Constraints.mustBeSignedBy (tsSeller ts)
+                                 , State p v
+                                 )
+    (v, p, AddTokens n)  -> Just ( mempty
+                                 , State p $ v <> assetClassValue (tsToken ts) n
+                                 )
+    (v, p, BuyTokens n)  -> Just ( mempty
+                                 , State p $ v <> assetClassValue (tsToken ts) (negate n) <> lovelaceValueOf (n * p)
+                                 )
+    (v, p, Withdraw n l) -> Just ( Constraints.mustBeSignedBy (tsSeller ts)
+                                 , State p $ v <> assetClassValue (tsToken ts) (negate n) <> lovelaceValueOf (negate l)
+                                 )
+
+{-# INLINABLE tsStateMachine #-}
+tsStateMachine :: TokenSale -> StateMachine Integer TSRedeemer
+tsStateMachine ts = mkStateMachine (Just $ tsNFT ts) (transition ts) (const False)
+
+{-# INLINABLE mkTSValidator #-}
+mkTSValidator :: TokenSale -> Integer -> TSRedeemer -> ScriptContext -> Bool
+mkTSValidator = mkValidator . tsStateMachine
+
+type TS = StateMachine Integer TSRedeemer
+
+tsInst :: TokenSale -> Scripts.ScriptInstance TS
+tsInst ts = Scripts.validator @TS
+    ($$(PlutusTx.compile [|| mkTSValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode ts)
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @Integer @TSRedeemer
+
+tsValidator :: TokenSale -> Validator
+tsValidator = Scripts.validatorScript . tsInst
+
+tsAddress :: TokenSale -> Ledger.Address
+tsAddress = scriptAddress . tsValidator
+
+tsClient :: TokenSale -> StateMachineClient Integer TSRedeemer
+tsClient ts = mkStateMachineClient $ StateMachineInstance (tsStateMachine ts) (tsInst ts)
+
+{-
+
+
+
+
+data FirstParams = FirstParams
+    { fpSecond         :: !PubKeyHash
+    , fpStake          :: !Integer
+    , fpPlayDeadline   :: !Slot
+    , fpRevealDeadline :: !Slot
+    , fpNonce          :: !ByteString
+    , fpCurrency       :: !CurrencySymbol
+    , fpTokenName      :: !TokenName
+    , fpChoice         :: !GameChoice
+    } deriving (Show, Generic, FromJSON, ToJSON, ToSchema)
+
+mapError' :: Contract w s SMContractError a -> Contract w s Text a
+mapError' = mapError $ pack . show
+
+firstGame :: forall w s. HasBlockchainActions s => FirstParams -> Contract w s Text ()
+firstGame fp = do
+    pkh <- pubKeyHash <$> Contract.ownPubKey
+    let game   = Game
+            { gFirst          = pkh
+            , gSecond         = fpSecond fp
+            , gStake          = fpStake fp
+            , gPlayDeadline   = fpPlayDeadline fp
+            , gRevealDeadline = fpRevealDeadline fp
+            , gToken          = AssetClass (fpCurrency fp, fpTokenName fp)
+            }
+        client = gameClient game
+        v      = lovelaceValueOf (fpStake fp)
+        c      = fpChoice fp
+        bs     = sha2_256 $ fpNonce fp `concatenate` if c == Zero then bsZero else bsOne
+    void $ mapError' $ runInitialise client (GameDatum bs Nothing) v
+    logInfo @String $ "made first move: " ++ show (fpChoice fp)
+
+    void $ awaitSlot $ 1 + fpPlayDeadline fp
+
+    m <- mapError' $ getOnChainState client
+    case m of
+        Nothing             -> throwError "game output not found"
+        Just ((o, _), _) -> case tyTxOutData o of
+
+            GameDatum _ Nothing -> do
+                logInfo @String "second player did not play"
+                void $ mapError' $ runStep client ClaimFirst
+                logInfo @String "first player reclaimed stake"
+
+            GameDatum _ (Just c') | c' == c -> do
+                logInfo @String "second player played and lost"
+                void $ mapError' $ runStep client $ Reveal $ fpNonce fp
+                logInfo @String "first player revealed and won"
+
+            _ -> logInfo @String "second player played and won"
+
+data SecondParams = SecondParams
+    { spFirst          :: !PubKeyHash
+    , spStake          :: !Integer
+    , spPlayDeadline   :: !Slot
+    , spRevealDeadline :: !Slot
+    , spCurrency       :: !CurrencySymbol
+    , spTokenName      :: !TokenName
+    , spChoice         :: !GameChoice
+    } deriving (Show, Generic, FromJSON, ToJSON, ToSchema)
+
+secondGame :: forall w s. HasBlockchainActions s => SecondParams -> Contract w s Text ()
+secondGame sp = do
+    pkh <- pubKeyHash <$> Contract.ownPubKey
+    let game   = Game
+            { gFirst          = spFirst sp
+            , gSecond         = pkh
+            , gStake          = spStake sp
+            , gPlayDeadline   = spPlayDeadline sp
+            , gRevealDeadline = spRevealDeadline sp
+            , gToken          = AssetClass (spCurrency sp, spTokenName sp)
+            }
+        client = gameClient game
+    m <- mapError' $ getOnChainState client
+    case m of
+        Nothing          -> logInfo @String "no running game found"
+        Just ((o, _), _) -> case tyTxOutData o of
+            GameDatum _ Nothing -> do
+                logInfo @String "running game found"
+                void $ mapError' $ runStep client $ Play $ spChoice sp
+                logInfo @String $ "made second move: " ++ show (spChoice sp)
+
+                void $ awaitSlot $ 1 + spRevealDeadline sp
+
+                m' <- mapError' $ getOnChainState client
+                case m' of
+                    Nothing -> logInfo @String "first player won"
+                    Just _  -> do
+                        logInfo @String "first player didn't reveal"
+                        void $ mapError' $ runStep client ClaimSecond
+                        logInfo @String "second player won"
+
+            _ -> throwError "unexpected datum"
+
+type GameSchema = BlockchainActions .\/ Endpoint "first" FirstParams .\/ Endpoint "second" SecondParams
+
+endpoints :: Contract () GameSchema Text ()
+endpoints = (first `select` second) >> endpoints
+  where
+    first  = endpoint @"first"  >>= firstGame
+    second = endpoint @"second" >>= secondGame
+-}
