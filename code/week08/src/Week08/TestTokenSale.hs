0e622f33c66fd28b1213f4515c90b844f470bab7
Lars Brünjes <brunjlar@gmail.com>
2021-05-23 16:37:21 +0200

    complete version of the token sales contract

---
+++ code/week08/src/Week08/TestTokenSale.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module Week08.TestTokenSale where
+
+import           Control.Monad                hiding (fmap)
+import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Monoid                  (Last (..))
+import           Data.Text                    (Text, pack)
+import           GHC.Generics                 (Generic)
+import           Plutus.Contract              as Contract hiding (when)
+import           Plutus.Contract.StateMachine
+import           Plutus.Contract.Test
+import qualified Plutus.Contracts.Currency    as C
+import qualified PlutusTx
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
+import           Ledger                       hiding (singleton)
+import           Ledger.Ada                   as Ada
+import           Ledger.Constraints           as Constraints
+import qualified Ledger.Typed.Scripts         as Scripts
+import           Ledger.Value
+import           Prelude                      (Semigroup (..))
+import qualified Prelude
+
+import           Week08.TokenSale
---
+++ code/week08/src/Week08/TokenSale.hs
@@ -0,0 +1,160 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module Week08.TokenSale
+    ( TokenSale (..)
+    , TSStartSchema
+    , TSUseSchema
+    , startTS'
+    , useTS
+    ) where
+
+import           Control.Monad                hiding (fmap)
+import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Monoid                  (Last (..))
+import           Data.Text                    (Text, pack)
+import           GHC.Generics                 (Generic)
+import           Plutus.Contract              as Contract hiding (when)
+import           Plutus.Contract.StateMachine
+import qualified Plutus.Contracts.Currency    as C
+import qualified PlutusTx
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
+import           Ledger                       hiding (singleton)
+import           Ledger.Ada                   as Ada
+import           Ledger.Constraints           as Constraints
+import qualified Ledger.Typed.Scripts         as Scripts
+import           Ledger.Value
+import           Prelude                      (Semigroup (..))
+import qualified Prelude
+
+data TokenSale = TokenSale
+    { tsSeller :: !PubKeyHash
+    , tsToken  :: !AssetClass
+    , tsNFT    :: !AssetClass
+    } deriving (Show, Generic, FromJSON, ToJSON, Prelude.Eq, Prelude.Ord)
+
+PlutusTx.makeLift ''TokenSale
+
+data TSRedeemer =
+      SetPrice Integer
+    | AddTokens Integer
+    | BuyTokens Integer
+    | Withdraw Integer Integer
+    deriving Show
+
+PlutusTx.unstableMakeIsData ''TSRedeemer
+
+{-# INLINABLE lovelaces #-}
+lovelaces :: Value -> Integer
+lovelaces = Ada.getLovelace . Ada.fromValue
+
+{-# INLINABLE transition #-}
+transition :: TokenSale -> State Integer -> TSRedeemer -> Maybe (TxConstraints Void Void, State Integer)
+transition ts s r = case (stateValue s, stateData s, r) of
+    (v, _, SetPrice p)   -> Just ( Constraints.mustBeSignedBy (tsSeller ts)
+                                 , State p (v <> nft (negate 1))
+                                 )
+    (v, p, AddTokens n)  -> Just ( mempty
+                                 , State p $ v <> nft (negate 1) <> assetClassValue (tsToken ts) n
+                                 )
+    (v, p, BuyTokens n)  -> Just ( mempty
+                                 , State p $ v <> nft (negate 1) <> assetClassValue (tsToken ts) (negate n) <> lovelaceValueOf (n * p)
+                                 )
+    (v, p, Withdraw n l) -> Just ( Constraints.mustBeSignedBy (tsSeller ts)
+                                 , State p $ v <> nft (negate 1) <> assetClassValue (tsToken ts) (negate n) <> lovelaceValueOf (negate l)
+                                 )
+  where
+    nft :: Integer -> Value
+    nft = assetClassValue (tsNFT ts)
+
+{-# INLINABLE tsStateMachine #-}
+tsStateMachine :: TokenSale -> StateMachine Integer TSRedeemer
+tsStateMachine ts = mkStateMachine (Just $ tsNFT ts) (transition ts) (const False)
+
+{-# INLINABLE mkTSValidator #-}
+mkTSValidator :: TokenSale -> Integer -> TSRedeemer -> ScriptContext -> Bool
+mkTSValidator = mkValidator . tsStateMachine
+
+type TS = StateMachine Integer TSRedeemer
+
+tsInst :: TokenSale -> Scripts.ScriptInstance TS
+tsInst ts = Scripts.validator @TS
+    ($$(PlutusTx.compile [|| mkTSValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode ts)
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @Integer @TSRedeemer
+
+tsValidator :: TokenSale -> Validator
+tsValidator = Scripts.validatorScript . tsInst
+
+tsAddress :: TokenSale -> Ledger.Address
+tsAddress = scriptAddress . tsValidator
+
+tsClient :: TokenSale -> StateMachineClient Integer TSRedeemer
+tsClient ts = mkStateMachineClient $ StateMachineInstance (tsStateMachine ts) (tsInst ts)
+
+mapErrorC :: Contract w s C.CurrencyError a -> Contract w s Text a
+mapErrorC = mapError $ pack . show
+
+mapErrorSM :: Contract w s SMContractError a -> Contract w s Text a
+mapErrorSM = mapError $ pack . show
+
+
+nftName :: TokenName
+nftName = "NFT"
+
+startTS :: HasBlockchainActions s => AssetClass -> Contract (Last TokenSale) s Text ()
+startTS token = do
+    pkh <- pubKeyHash <$> Contract.ownPubKey
+    osc <- mapErrorC $ C.forgeContract pkh [(nftName, 1)]
+    let ts = TokenSale
+            { tsSeller = pkh
+            , tsToken  = token
+            , tsNFT    = AssetClass (C.currencySymbol osc, nftName)
+            }
+        client = tsClient ts
+    void $ mapErrorSM $ runInitialise client 0 mempty
+    tell $ Last $ Just ts
+    logInfo $ "started token sale " ++ show ts
+
+setPrice :: HasBlockchainActions s => TokenSale -> Integer -> Contract w s Text ()
+setPrice ts p = void $ mapErrorSM $ runStep (tsClient ts) $ SetPrice p
+
+addTokens :: HasBlockchainActions s => TokenSale -> Integer -> Contract w s Text ()
+addTokens ts n = void $ mapErrorSM $ runStep (tsClient ts) $ AddTokens n
+
+buyTokens :: HasBlockchainActions s => TokenSale -> Integer -> Contract w s Text ()
+buyTokens ts n = void $ mapErrorSM $ runStep (tsClient ts) $ BuyTokens n
+
+withdraw :: HasBlockchainActions s => TokenSale -> Integer -> Integer -> Contract w s Text ()
+withdraw ts n l = void $ mapErrorSM $ runStep (tsClient ts) $ Withdraw n l
+
+type TSStartSchema = BlockchainActions .\/ Endpoint "start" (CurrencySymbol, TokenName)
+type TSUseSchema = BlockchainActions
+    .\/ Endpoint "set price"  Integer
+    .\/ Endpoint "add tokens" Integer
+    .\/ Endpoint "buy tokens" Integer
+    .\/ Endpoint "withdraw"   (Integer, Integer)
+
+startTS' :: Contract (Last TokenSale) TSStartSchema Text ()
+startTS' = start >> startTS'
+  where
+    start  = endpoint @"start"  >>= startTS . AssetClass
+
+useTS :: TokenSale -> Contract () TSUseSchema Text ()
+useTS ts = (setPrice' `select` addTokens' `select` buyTokens' `select` withdraw') >> useTS ts
+  where
+    setPrice'  = handleError logError $ endpoint @"set price"  >>= setPrice ts
+    addTokens' = handleError logError $ endpoint @"add tokens" >>= addTokens ts
+    buyTokens' = handleError logError $ endpoint @"buy tokens" >>= buyTokens ts
+    withdraw'  = handleError logError $ endpoint @"withdraw"   >>= uncurry (withdraw ts)
