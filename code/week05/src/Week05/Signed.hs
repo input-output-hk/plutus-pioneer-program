c74fb6fefa1616ac0684560fd5ad031d0ffaf1c7
Lars Brünjes <brunjlar@gmail.com>
2021-07-24 19:27:23 +0200

    small update

--- code/week05/src/Week05/Signed.hs
+++ code/week05/src/Week05/Signed.hs
@@ -12,24 +12,24 @@
 
 module Week05.Signed where
 
-import           Control.Monad              hiding (fmap)
-import           Data.Aeson                 (ToJSON, FromJSON)
-import           Data.Text                  (Text)
-import           Data.Void                  (Void)
-import           GHC.Generics               (Generic)
-import           Plutus.Contract            as Contract
-import           Plutus.Trace.Emulator      as Emulator
+import           Control.Monad          hiding (fmap)
+import           Data.Aeson             (ToJSON, FromJSON)
+import           Data.Text              (Text)
+import           Data.Void              (Void)
+import           GHC.Generics           (Generic)
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
 import qualified PlutusTx
-import           PlutusTx.Prelude           hiding (Semigroup(..), unless)
-import           Ledger                     hiding (mint, singleton)
-import           Ledger.Constraints         as Constraints
-import qualified Ledger.Typed.Scripts       as Scripts
-import           Ledger.Value               as Value
-import           Playground.Contract        (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
-import           Playground.TH              (mkKnownCurrencies, mkSchemaDefinitions)
-import           Playground.Types           (KnownCurrency (..))
-import           Prelude                    (IO, Show (..), String)
-import           Text.Printf                (printf)
+import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Ledger                 hiding (mint, singleton)
+import           Ledger.Constraints     as Constraints
+import qualified Ledger.Typed.Scripts   as Scripts
+import           Ledger.Value           as Value
+import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
+import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types       (KnownCurrency (..))
+import           Prelude                (IO, Show (..), String)
+import           Text.Printf            (printf)
 import           Wallet.Emulator.Wallet
 
 {-# INLINABLE mkPolicy #-}
@@ -40,7 +40,7 @@ policy :: PubKeyHash -> Scripts.MintingPolicy
 policy pkh = mkMintingPolicyScript $
     $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . mkPolicy ||])
     `PlutusTx.applyCode`
-    PlutusTx.liftCode pkh
+    (PlutusTx.liftCode pkh)
 
 curSymbol :: PubKeyHash -> CurrencySymbol
 curSymbol = scriptCurrencySymbol . policy
