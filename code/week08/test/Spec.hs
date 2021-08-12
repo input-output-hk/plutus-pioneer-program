681de03b9af2a777d4df4269fa67acdf7fab0573
Lars Brünjes <brunjlar@gmail.com>
2021-08-12 09:37:52 +0200

    removed solutions

--- code/week08/test/Spec.hs
+++ code/week08/test/Spec.hs
@@ -3,9 +3,7 @@ module Main
     ) where
 
 import qualified Spec.Model
-import qualified Spec.ModelWithClose
 import qualified Spec.Trace
-import qualified Spec.TraceWithClose
 import           Test.Tasty
 
 main :: IO ()
@@ -14,7 +12,5 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "token sale"
     [ Spec.Trace.tests
-    , Spec.TraceWithClose.tests
     , Spec.Model.tests
-    , Spec.ModelWithClose.tests
     ]
