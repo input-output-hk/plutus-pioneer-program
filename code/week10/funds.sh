db9a7a514b561c0c4a52ac22a653cab2abe61fd6
Lars Brünjes <brunjlar@gmail.com>
2021-08-18 19:03:22 +0200

    updated shell scripts

--- code/week10/funds.sh
+++ code/week10/funds.sh
@@ -1,4 +1,4 @@
 #1/bin/sh
-curl  "http://localhost:8080/api/new/contract/instance/$(cat W$1.cid)/endpoint/funds" \
+curl  "http://localhost:9080/api/contract/instance/$(cat W$1.cid)/endpoint/funds" \
 --header 'Content-Type: application/json' \
 --data-raw '[]'
--- code/week10/logs.sh
+++ code/week10/logs.sh
@@ -1,2 +1,2 @@
 #1/bin/sh
-curl  "http://localhost:8080/api/new/contract/instance/$(cat W$1.cid)/status" | jq ".cicCurrentState.logs"
+curl  "http://localhost:9080/api/contract/instance/$(cat W$1.cid)/status" | jq ".cicCurrentState.logs"
