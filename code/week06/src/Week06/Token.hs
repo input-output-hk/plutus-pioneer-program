159f42e6f8fa6cc3ad317c08579396b20d4a84ae
Lars Brünjes <brunjlar@gmail.com>
2022-02-22 22:49:14 +0100

    minting working

--- code/week06/src/Week06/Token.hs
+++ code/week06/src/Week06/Token.hs
@@ -20,15 +20,14 @@ module Week06.Token
 
 import           Control.Monad               hiding (fmap)
 import           Data.Aeson                  (FromJSON, ToJSON)
-import           Data.Function               (on)
-import           Data.List                   (maximumBy)
 import qualified Data.Map                    as Map
+import           Data.Maybe                  (fromJust)
 import           Data.OpenApi.Schema         (ToSchema)
 import           Data.Text                   (Text, pack)
 import           Data.Void                   (Void)
 import           GHC.Generics                (Generic)
 import           Plutus.Contract             as Contract
-import           Plutus.V1.Ledger.Ada        (Ada (..), fromValue, lovelaceValueOf)
+import           Plutus.Contract.Wallet      (getUnspentOutput)
 import           Plutus.V1.Ledger.Credential
 import qualified PlutusTx
 import           PlutusTx.Prelude            hiding (Semigroup(..), unless)
@@ -75,22 +74,6 @@ data TokenParams = TokenParams
     , tpAddress :: !Address
     } deriving (Prelude.Eq, Prelude.Ord, Generic, FromJSON, ToJSON, ToSchema, Show)
 
-getLargestAdaUTxO :: Address -> Contract w s Text (TxOutRef, ChainIndexTxOut, Integer)
-getLargestAdaUTxO addr = do
-    Contract.logDebug @String $ printf "started getting largest ADA UTxO at address %s" $ show addr
-    utxos <- map (\(oref, o) -> (oref, o, getLovelace $ fromValue $ _ciTxOutValue o)) . Map.toList . Map.filter hasOnlyAda <$> utxosAt addr
-    case utxos of
-        []    -> Contract.throwError $ pack $ printf "no ADA UTxO found at address %s" $ show addr
-        _ : _ -> do
-            let (oref, o, n) = maximumBy (compare `on` (\(_, _, n') -> n')) utxos
-            Contract.logDebug @String $ printf "found UTxO at %s containing %d lovelace" (show oref) n
-            return (oref, o, n)
-  where
-    hasOnlyAda :: ChainIndexTxOut -> Bool
-    hasOnlyAda o = case flattenValue $ _ciTxOutValue o of
-        [_] -> True
-        _   -> False
-
 mintToken :: TokenParams -> Contract w s Text CurrencySymbol
 mintToken tp = do
     Contract.logDebug @String $ printf "started minting: %s" $ show tp
@@ -98,32 +81,30 @@ mintToken tp = do
     case getCredentials addr of
         Nothing      -> Contract.throwError $ pack $ printf "expected pubkey address, but got %s" $ show addr
         Just (x, my) -> do
-            (oref, o, _) <- getLargestAdaUTxO addr
-            let tn     = tpToken tp
-                amt    = tpAmount tp
-                cs     = tokenCurSymbol oref tn amt
-                val    = Value.singleton cs tn amt
-                val'   = val <> lovelaceValueOf 2_000_000
-                (l, c) = case my of
-                    Nothing -> ( Constraints.ownPaymentPubKeyHash x
-                               , Constraints.mustPayToPubKey x val'
-                               )
-                    Just y  -> ( Constraints.ownPaymentPubKeyHash x <> Constraints.ownStakePubKeyHash y
-                               , Constraints.mustPayToPubKeyAddress x y val'
-                               )
-            let lookups     = Constraints.mintingPolicy (tokenPolicy oref tn amt) <>
-                              Constraints.unspentOutputs (Map.singleton oref o)   <>
-                              l
+            oref <- getUnspentOutput
+            o    <- fromJust <$> Contract.txOutFromRef oref
+            Contract.logDebug @String $ printf "picked UTxO at %s with value %s" (show oref) (show $ _ciTxOutValue o)
+
+            let tn          = tpToken tp
+                amt         = tpAmount tp
+                cs          = tokenCurSymbol oref tn amt
+                val         = Value.singleton cs tn amt
+                c           = case my of
+                    Nothing -> Constraints.mustPayToPubKey x val
+                    Just y  -> Constraints.mustPayToPubKeyAddress x y val
+                lookups     = Constraints.mintingPolicy (tokenPolicy oref tn amt) <>
+                              Constraints.unspentOutputs (Map.singleton oref o)
                 constraints = Constraints.mustMintValue val          <>
                               Constraints.mustSpendPubKeyOutput oref <>
                               c
-            unbalanced <- mkTxConstraints @Void lookups constraints
+
+            unbalanced <- adjustUnbalancedTx <$> mkTxConstraints @Void lookups constraints
             Contract.logDebug @String $ printf "unbalanced: %s" $ show unbalanced
 
-            unsigned   <- balanceTx unbalanced
+            unsigned <- balanceTx unbalanced
             Contract.logDebug @String $ printf "balanced: %s" $ show unsigned
 
-            signed     <- submitBalancedTx unsigned
+            signed <- submitBalancedTx unsigned
             Contract.logDebug @String $ printf "signed: %s" $ show signed
 
             Contract.logInfo @String $ printf "minted %s" (show val)
