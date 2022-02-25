cb7e1f19d4ae513a161e13e4e0eb3c8a4ebeac3c
Lars Brünjes <brunjlar@gmail.com>
2022-02-25 09:42:52 +0100

    added definitions to PAB

--- code/week06/src/Week06/PAB.hs
+++ code/week06/src/Week06/PAB.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE DeriveAnyClass   #-}
-{-# LANGUAGE DeriveGeneric    #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Week06.PAB
     ( Address
-    , TokenParams (..)
     , TokenContracts (..)
     ) where
 
@@ -14,10 +14,10 @@ import           GHC.Generics                        (Generic)
 import           Ledger                              (Address)
 import           Plutus.PAB.Effects.Contract.Builtin (Empty, HasDefinitions (..), SomeBuiltin (..), endpointsToSchemas)
 import           Prettyprinter                       (Pretty (..), viaShow)
+import           Wallet.Emulator.Wallet              (knownWallet, mockWalletAddress)
 
 import qualified Week06.Monitor                      as Monitor
 import qualified Week06.Token.OffChain               as Token
-import Week06.Token.OffChain (TokenParams(TokenParams))
 
 data TokenContracts = Mint Token.TokenParams | Monitor Address
     deriving (Eq, Ord, Show, Generic, FromJSON, ToJSON, ToSchema)
@@ -27,10 +27,21 @@ instance Pretty TokenContracts where
 
 instance HasDefinitions TokenContracts where
 
-    getDefinitions        = []
+    getDefinitions        = [Mint exampleTP, Monitor exampleAddr]
 
     getContract (Mint tp)      = SomeBuiltin $ Token.mintToken @() @Empty tp
     getContract (Monitor addr) = SomeBuiltin $ Monitor.monitor addr
 
     getSchema (Mint _)    = endpointsToSchemas @Empty
     getSchema (Monitor _) = endpointsToSchemas @Empty
+
+
+exampleAddr :: Address
+exampleAddr = mockWalletAddress $ knownWallet 1
+
+exampleTP :: Token.TokenParams
+exampleTP = Token.TokenParams
+    { Token.tpAddress = exampleAddr
+    , Token.tpAmount  = 123456
+    , Token.tpToken   = "PPP"
+    }
