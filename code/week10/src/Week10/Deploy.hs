e3089a5faa1f31c6f0d0f4b45dc97ae39eab1085
Lars Brünjes <brunjlar@gmail.com>
2022-03-30 21:05:53 +0200

    simplified code

--- code/week10/src/Week10/Deploy.hs
+++ code/week10/src/Week10/Deploy.hs
@@ -3,9 +3,7 @@
 {-# LANGUAGE TypeApplications  #-}
 
 module Week10.Deploy
-    ( writeJSON
-    , writeUnit
-    , writeStakeValidator
+    ( writeStakeValidator
     , tryReadAddress
     ) where
 
@@ -17,34 +15,18 @@ import           Cardano.Ledger.Crypto       (StandardCrypto)
 import           Cardano.Ledger.Hashes       (ScriptHash (..))
 import           Cardano.Ledger.Keys         (KeyHash (..))
 import           Codec.Serialise             (serialise)
-import           Data.Aeson                  (encode)
 import qualified Data.ByteString.Lazy        as LBS
 import qualified Data.ByteString.Short       as SBS
 import           Data.Text                   (pack)
 import           Plutus.V1.Ledger.Credential as Plutus
 import           Plutus.V1.Ledger.Crypto     as Plutus
-import           PlutusTx                    (Data (..))
-import qualified PlutusTx
 import           PlutusTx.Builtins           (toBuiltin)
 import qualified Ledger                      as Plutus
 
 import           Week10.Staking
 
-dataToScriptData :: Data -> ScriptData
-dataToScriptData (Constr n xs) = ScriptDataConstructor n $ dataToScriptData <$> xs
-dataToScriptData (Map xs)      = ScriptDataMap [(dataToScriptData x, dataToScriptData y) | (x, y) <- xs]
-dataToScriptData (List xs)     = ScriptDataList $ dataToScriptData <$> xs
-dataToScriptData (I n)         = ScriptDataNumber n
-dataToScriptData (B bs)        = ScriptDataBytes bs
-
-writeJSON :: PlutusTx.ToData a => FilePath -> a -> IO ()
-writeJSON file = LBS.writeFile file . encode . scriptDataToJson ScriptDataJsonDetailedSchema . dataToScriptData . PlutusTx.toData
-
 writeStakeValidator :: FilePath -> Plutus.Address -> IO (Either (FileError ()) ())
 writeStakeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Plutus.getStakeValidator . stakeValidator
-
-writeUnit :: FilePath -> IO ()
-writeUnit file = writeJSON file ()
 
 credentialLedgerToPlutus :: Ledger.Credential a StandardCrypto -> Plutus.Credential
 credentialLedgerToPlutus (ScriptHashObj (ScriptHash h)) = Plutus.ScriptCredential $ Plutus.ValidatorHash $ toBuiltin $ hashToBytes h
