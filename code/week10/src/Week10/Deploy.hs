95503331f2e4ce89599f4a211b6dca993bde0791
Lars Brünjes <brunjlar@gmail.com>
2022-03-30 18:46:07 +0200

    executable to write validator script

--- code/week10/src/Week10/Deploy.hs
+++ code/week10/src/Week10/Deploy.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs             #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications  #-}
 
@@ -5,17 +6,27 @@ module Week10.Deploy
     ( writeJSON
     , writeUnit
     , writeStakeValidator
+    , tryReadAddress
     ) where
 
-import           Cardano.Api
-import           Cardano.Api.Shelley   (PlutusScript (..))
-import           Codec.Serialise       (serialise)
-import           Data.Aeson            (encode)
-import qualified Data.ByteString.Lazy  as LBS
-import qualified Data.ByteString.Short as SBS
-import           PlutusTx              (Data (..))
+import           Cardano.Api                 as API
+import           Cardano.Api.Shelley         (Address (..), PlutusScript (..))
+import           Cardano.Crypto.Hash.Class   (hashToBytes)
+import           Cardano.Ledger.Credential   as Ledger
+import           Cardano.Ledger.Crypto       (StandardCrypto)
+import           Cardano.Ledger.Hashes       (ScriptHash (..))
+import           Cardano.Ledger.Keys         (KeyHash (..))
+import           Codec.Serialise             (serialise)
+import           Data.Aeson                  (encode)
+import qualified Data.ByteString.Lazy        as LBS
+import qualified Data.ByteString.Short       as SBS
+import           Data.Text                   (pack)
+import           Plutus.V1.Ledger.Credential as Plutus
+import           Plutus.V1.Ledger.Crypto     as Plutus
+import           PlutusTx                    (Data (..))
 import qualified PlutusTx
-import qualified Ledger
+import           PlutusTx.Builtins           (toBuiltin)
+import qualified Ledger                      as Plutus
 
 import           Week10.Staking
 
@@ -29,13 +40,26 @@ dataToScriptData (B bs)        = ScriptDataBytes bs
 writeJSON :: PlutusTx.ToData a => FilePath -> a -> IO ()
 writeJSON file = LBS.writeFile file . encode . scriptDataToJson ScriptDataJsonDetailedSchema . dataToScriptData . PlutusTx.toData
 
-writeStakeValidator :: FilePath -> Ledger.Address -> IO (Either (FileError ()) ())
-writeStakeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Ledger.getStakeValidator . stakeValidator
+writeStakeValidator :: FilePath -> Plutus.Address -> IO (Either (FileError ()) ())
+writeStakeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Plutus.getStakeValidator . stakeValidator
 
-writeUnit :: IO ()
-writeUnit = writeJSON "testnet/unit.json" ()
+writeUnit :: FilePath -> IO ()
+writeUnit file = writeJSON file ()
 
-{-
-writeVestingValidator :: IO (Either (FileError ()) ())
-writeVestingValidator = writeValidator "testnet/vesting.plutus" validator
--}
+credentialLedgerToPlutus :: Ledger.Credential a StandardCrypto -> Plutus.Credential
+credentialLedgerToPlutus (ScriptHashObj (ScriptHash h)) = Plutus.ScriptCredential $ Plutus.ValidatorHash $ toBuiltin $ hashToBytes h
+credentialLedgerToPlutus (KeyHashObj (KeyHash h))       = Plutus.PubKeyCredential $ Plutus.PubKeyHash $ toBuiltin $ hashToBytes h
+
+stakeReferenceLedgerToPlutus :: Ledger.StakeReference StandardCrypto -> Maybe Plutus.StakingCredential
+stakeReferenceLedgerToPlutus (StakeRefBase x)                   = Just $ StakingHash $ credentialLedgerToPlutus x
+stakeReferenceLedgerToPlutus (StakeRefPtr (Ptr (SlotNo x) y z)) = Just $ StakingPtr (fromIntegral x) (fromIntegral y) (fromIntegral z)
+stakeReferenceLedgerToPlutus StakeRefNull                       = Nothing
+
+tryReadAddress :: String -> Maybe Plutus.Address
+tryReadAddress x = case deserialiseAddress AsAddressAny $ pack x of
+    Nothing                                      -> Nothing
+    Just (AddressByron _)                        -> Nothing
+    Just (AddressShelley (ShelleyAddress _ p s)) -> Just Plutus.Address
+        { Plutus.addressCredential        = credentialLedgerToPlutus p
+        , Plutus.addressStakingCredential = stakeReferenceLedgerToPlutus s
+        }
