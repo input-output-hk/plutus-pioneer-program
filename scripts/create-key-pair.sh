bde30d3bd70657e7e3bde4265497b858d03942bd
Lars Brünjes <brunjlar@gmail.com>
2023-02-27 20:54:59 +0000

    added script to create key-pair

--- scripts/create-key-pair.sh
+++ scripts/create-key-pair.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-echo "name?"
-read name
+
+if [ -z "$1" ]; then
+    >&2 echo "expected name as argument"
+    exit 1
+fi
 
 path=/workspaces/plutus-pioneer-program/keys
-vkey="$path/$name.vkey"
-skey="$path/$name.skey"
-addr="$path/$name.addr"
+mkdir -p "$path"
 
-cardano-cli address key-gen --verification-key-file "$vkey" --signing-key-file "$skey"
+vkey="$path/$1.vkey"
+skey="$path/$1.skey"
+addr="$path/$1.addr"
+
+if [ -f "$vkey" ]; then
+    >&2 echo "verification key file $vkey already exists"
+    exit 1
+fi
+
+if [ -f "$skey" ]; then
+    >&2 echo "signing key file $skey already exists"
+    exit 1
+fi
+
+if [ -f "$addr" ]; then
+    >&2 echo "address file $addr already exists"
+    exit 1
+fi
+
+cardano-cli address key-gen --verification-key-file "$vkey" --signing-key-file "$skey" &&
 cardano-cli address build --payment-verification-key-file "$vkey" --testnet-magic 2 --out-file "$addr"
 
 echo "wrote verification key to: $vkey"
