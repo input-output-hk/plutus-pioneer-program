0848b494a6bcdea97fd15dbea0d87979faa1fa91
Lars Brünjes <brunjlar@gmail.com>
2023-02-28 10:44:10 +0000

    updated doc alias for Demeter

--- scripts/alias-serve-docs-demeter.sh
+++ scripts/alias-serve-docs-demeter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "alias serve-docs='python3 -m http.server -d /config/workspace/repo/docs/plutus-docs'" >> ~/.bashrc
+echo "alias serve-docs='python3 -m http.server -d /config/workspace/repo/docs/plutus-docs/haddock'" >> ~/.bashrc
