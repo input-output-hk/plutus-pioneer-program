16135e5f28e8cd1b40a89c4ba8450ec50d392db8
perturbing <thomas.vellekoop@iohk.io>
2023-03-23 10:49:37 +0100

    clean up lecture file

--- code/Week04/lecture/lucid-vesting.ts
+++ code/Week04/lecture/lucid-vesting.ts
@@ -10,13 +10,14 @@ import {
     Address,
     AddressDetails,
 } from "https://deno.land/x/lucid@0.9.1/mod.ts"
+// create a seed.ts file with your seed
 import { secretSeed } from "./seed.ts"
 
 // set blockfrost endpoint
 const lucid = await Lucid.new(
   new Blockfrost(
     "https://cardano-preprod.blockfrost.io/api/v0",
-    "preprod3eBzCw9lDVA6DTQMUqxri7Ne2q4u1cCd"
+    "insert you own api key here"
   ),
   "Preprod"
 );
