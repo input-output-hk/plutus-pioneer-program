b9bedeb14d14f7de09d1a9d7a28d6439b47898b2
luisantonioig <antonio.ibarra.g46@gmail.com>
2023-04-03 17:54:51 -0600

    Homework for Week06

--- code/Week06/lecture/Swap.hs
+++ code/Week06/lecture/Swap.hs
@@ -7,24 +7,35 @@
 module Swap where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
-import           PlutusTx             (compile)
-import PlutusTx.Builtins ( BuiltinData )
-import PlutusTx.Prelude ( Bool (..) ) 
+import           PlutusTx             (compile, unstableMakeIsData)
+import PlutusTx.Builtins ( BuiltinData , Integer )
+import PlutusTx.Prelude ( Bool (..), (==) )
 import Plutus.V2.Ledger.Api (ScriptContext)
+import Plutus.V2.Ledger.Contexts (valuePaidTo)
 import Utilities (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
 
--- TODO: implement the validator
+data DatumSwap = DatumSwap
+  { beneficiary :: PlutusV2.PubKeyHash
+  , price       :: Integer
+  }
+PlutusTx.unstableMakeIsData ''DatumSwap
 
 {-# INLINABLE mkValidator #-}
-mkValidator :: () -> () -> ScriptContext -> Bool
-mkValidator _ _ _ = True
+mkValidator :: DatumSwap -> () -> ScriptContext -> Bool
+mkValidator ds _ ctx = outputToBeneficiary
+  where
+    outputToBeneficiary = valuePaidTo txInfo (beneficiary ds) == PlutusV2.singleton PlutusV2.adaSymbol PlutusV2.adaToken (price ds)
+
+    txInfo = PlutusV2.scriptContextTxInfo ctx
+
 
 {-# INLINABLE  mkWrappedValidator #-}
 mkWrappedValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedValidator = wrapValidator mkValidator
+
 
 validator :: PlutusV2.Validator
 validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| mkWrappedValidator ||])
