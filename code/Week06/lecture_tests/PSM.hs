d7c002223f4201936bb7cc082c31ba58848dfe21
Robertino <48034748+rober-m@users.noreply.github.com>
2023-04-05 11:58:29 +0000

    recorded PSM video

--- code/Week06/lecture_tests/PSM.hs
+++ code/Week06/lecture_tests/PSM.hs
@@ -6,7 +6,6 @@ import           Prelude
 import           Test.Tasty           (defaultMain, testGroup)
 
 import           Control.Monad        (replicateM)
-import           Data.Functor         (void)
 import           Plutus.Model         (Ada (Lovelace), Run, ada, adaValue,
                                        defaultBabbage, mustFail, newUser,
                                        noErrors, sendValue, testNoErrors,
@@ -17,8 +16,7 @@ import           Plutus.V1.Ledger.Api (PubKeyHash)
 --------------------------------------- TESTING MAIN ----------------------------------------------
 
 main :: IO ()
-main = do
-  defaultMain $ do
+main = defaultMain $ do
     testGroup
       "Test simple user transactions"
       [ good "Simple spend" simpleSpend
@@ -36,7 +34,7 @@ setupUsers :: Run [PubKeyHash]
 setupUsers = replicateM 3 $ newUser $ ada (Lovelace 1000)
 
 ---------------------------------------------------------------------------------------------------
-------------------------------------- TESTING TRANSACTIONS -------------------------------------------
+------------------------------------- TESTING TRANSACTIONS ----------------------------------------
 
 -- Function to test that a simple transaction works
 simpleSpend :: Run Bool
@@ -47,7 +45,7 @@ simpleSpend = do
     sendValue u2 (adaValue 100) u3     -- Send 100 lovelaces from user 2 to user 3
     isOk <- noErrors                   -- Check that all TXs were accepted without errors
     vals <- mapM valueAt users         -- Read user values
-    pure $ isOk &&                     -- Check isOk and that all users have correct values
+    return $ isOk &&                     -- Check isOk and that all users have correct values
            (vals == fmap adaValue [900, 1000, 1100])
 
 -- Function to test that a transaction fails if there are not enough funds
@@ -55,5 +53,5 @@ notEnoughFunds :: Run Bool
 notEnoughFunds = do
   users <- setupUsers               -- Create 3 users and assign each 1000 lovelaces
   let [u1, u2, _u3] = users         -- Give names to individual users
-  void $ sendValue u1 (adaValue 10000) u2 -- Send 10.000 lovelaces from user 1 to user 2
+  sendValue u1 (adaValue 10000) u2  -- Send 10.000 lovelaces from user 1 to user 2
   noErrors  -- Check that all TXs were accepted without errors (should fail)
