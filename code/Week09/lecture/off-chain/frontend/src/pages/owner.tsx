f5d9572c9b0928e645efb59dba56d17fd5a33730
Robertino <48034748+rober-m@users.noreply.github.com>
2023-04-25 00:47:48 +0100

    made ui nice again

--- code/Week09/lecture/off-chain/frontend/src/pages/owner.tsx
+++ code/Week09/lecture/off-chain/frontend/src/pages/owner.tsx
@@ -7,135 +7,184 @@ import DeployScripts from "@/components/DeployScripts";
 import { HiUserCircle } from "react-icons/hi";
 import { IoReloadCircleSharp } from "react-icons/io5";
 import MintStablecoin from "@/components/MintStablecoin";
+import { useState } from "react";
 
 export default function Home() {
-    const { appState, setAppState } = useContext(AppStateContext);
-    const {
-        wAddr,
-        scPolicyIdHex,
-        scAssetClassHex,
-        oracleWithNftUTxO,
-        oracleAddress,
-        minPercent,
-        txCollScriptDeployment,
-        txMintingPolScriptDeployment,
-    } = appState;
+  const { appState, setAppState } = useContext(AppStateContext);
+  const {
+    wAddr,
+    scPolicyIdHex,
+    scAssetClassHex,
+    oracleWithNftUTxO,
+    oracleAddress,
+    minPercent,
+    txCollScriptDeployment,
+    txMintingPolScriptDeployment,
+  } = appState;
 
-    const refreshWallet = async () => {
-        if (!appState.lucid || !window.cardano.nami) return;
-        const nami = await window.cardano.nami.enable();
-        appState.lucid.selectWallet(nami);
-        setAppState({
-            ...appState,
-            wAddr: await appState.lucid.wallet.address(),
-        });
-    };
+  const refreshWallet = async () => {
+    if (!appState.lucid || !window.cardano.nami) return;
+    const nami = await window.cardano.nami.enable();
+    appState.lucid.selectWallet(nami);
+    setAppState({
+      ...appState,
+      wAddr: await appState.lucid.wallet.address(),
+    });
+  };
 
-    return (
-        <main className="flex min-h-screen w-screen h-screen gap-6 flex-row-reverse items-center justify-between p-5 bg-zinc-800">
-            <div className="flex flex-col items-center justify-start  w-[380px] mt-2">
-                {/* USER LOGGED */}
-                <div className="absolute justify-center items-center right-0 top-5 bg-zinc-50  h-12  w-48 rounded-l-2xl flex flex-row">
-                    <HiUserCircle
-                        className="text-4xl text-zinc-600"
-                        onClick={refreshWallet}
-                    />
-                    <p className="text-lg mx-2 text-zinc-800">Developer</p>
-                    <IoReloadCircleSharp
-                        className="text-3xl mx-2 text-zinc-600 active:text-zinc-800"
-                        onClick={refreshWallet}
-                    />
-                </div>
+  const [isPerson, setIsPerson] = useState<Person>("oracle");
+  type Person = "oracle" | "user" | "owner";
+  const handleClick = (v: Person) => {
+    if (v === "oracle") {
+      setIsPerson("oracle");
+    } else if (v === "user") {
+      setIsPerson("user");
+    } else {
+      setIsPerson("owner");
+    }
+    console.log(isPerson);
+  };
 
-                {/* INFORMATION TABLE */}
-                <p className=" overflow-clip self-start tracking-[0.2em]  text-xs text-zinc-200">
-                    INFO TABLE
-                </p>
+  return (
+    <main className="flex min-h-screen w-screen h-screen gap-6 flex-row-reverse items-center justify-between px-5 pb-5  pt-20 bg-zinc-800">
+      <div className="flex flex-col items-center justify-start  w-[380px] mt-2">
+        {/* USER LOGGED */}
+        <div className="absolute justify-center items-center right-0 top-5 bg-zinc-50  h-12  w-48 rounded-l-2xl flex flex-row">
+          <HiUserCircle
+            className="text-4xl text-zinc-600"
+            onClick={refreshWallet}
+          />
+          <p className="text-lg mx-2 text-zinc-800">Developer</p>
+          <IoReloadCircleSharp
+            className="text-3xl mx-2 text-zinc-600 active:text-zinc-800"
+            onClick={refreshWallet}
+          />
+        </div>
 
-                <div className=" overflow-hidden bg-zinc-50 rounded-lg w-full my-4 h-auto border border-spacing-1 border-zinc-50">
-                    <ExplorerLink
-                        message="Wallet: "
-                        type="address"
-                        value={
-                            wAddr
-                                ? `${wAddr.substring(
-                                      0,
-                                      15
-                                  )}...${wAddr.substring(100)}`
-                                : ""
-                        }
-                    />
-                    <ExplorerLink
-                        message="Oracle address: "
-                        type="address"
-                        value={oracleAddress || ""}
-                    />
+        {/* INFORMATION TABLE */}
+        <p className=" overflow-clip self-start tracking-[0.2em]  text-xs text-zinc-200">
+          INFO TABLE
+        </p>
 
-                    <ExplorerLink
-                        message="Oracle UTxO with NFT: "
-                        type="tx"
-                        value={
-                            oracleWithNftUTxO?.txHash
-                                ? `${oracleWithNftUTxO?.txHash}#${oracleWithNftUTxO?.outputIndex}`
-                                : ""
-                        }
-                    />
-                    <ExplorerLinkPrime
-                        message="Oracle's Datum (Price of ADA in cents): "
-                        type="datum"
-                        link={
-                            oracleWithNftUTxO?.txHash
-                                ? oracleWithNftUTxO?.txHash
-                                : "No TxHash"
-                        }
-                        value={
-                            oracleWithNftUTxO?.datum
-                                ? oracleWithNftUTxO?.datum
-                                : "No Datum"
-                        }
-                    />
-                    <ExplorerLink
-                        message="Tx that deployed the Collateral script: "
-                        type="tx"
-                        value={txCollScriptDeployment || ""}
-                    />
-                    <ExplorerLink
-                        message="Tx that deployed the Minting script: "
-                        type="tx"
-                        value={txMintingPolScriptDeployment || ""}
-                    />
-                    <ExplorerLink
-                        message="Stablecoin PolicyId in Hex:"
-                        type="policy"
-                        value={scPolicyIdHex || ""}
-                    />
-                    <ExplorerLink
-                        message="Stablecoin AssetClass in Hex:"
-                        type="asset"
-                        value={scAssetClassHex || ""}
-                    />
+        <div className=" overflow-hidden bg-zinc-50 rounded-lg w-full my-4 h-auto border border-spacing-1 border-zinc-50">
+          <ExplorerLink
+            message="Wallet: "
+            type="address"
+            value={
+              wAddr ? `${wAddr.substring(0, 15)}...${wAddr.substring(100)}` : ""
+            }
+          />
+          <ExplorerLink
+            message="Oracle address: "
+            type="address"
+            value={oracleAddress || ""}
+          />
 
-                    <div className="font-quicksand h-16 w-full overflow-hidden">
-                        <p className=" bg-zinc-800  text-base text-zinc-100 h-8 pt-[6px] pl-2">
-                            Depoyed with Minimum Percentage of Collateral:
-                        </p>{" "}
-                        {minPercent}
-                    </div>
-                </div>
-            </div>
+          <ExplorerLink
+            message="Oracle UTxO with NFT: "
+            type="tx"
+            value={
+              oracleWithNftUTxO?.txHash
+                ? `${oracleWithNftUTxO?.txHash}#${oracleWithNftUTxO?.outputIndex}`
+                : ""
+            }
+          />
+          <ExplorerLinkPrime
+            message="Oracle's Datum (Price of ADA in cents): "
+            type="datum"
+            link={
+              oracleWithNftUTxO?.txHash
+                ? oracleWithNftUTxO?.txHash
+                : "No TxHash"
+            }
+            value={
+              oracleWithNftUTxO?.datum ? oracleWithNftUTxO?.datum : "No Datum"
+            }
+          />
+          <ExplorerLink
+            message="Tx that deployed the Collateral script: "
+            type="tx"
+            value={txCollScriptDeployment || ""}
+          />
+          <ExplorerLink
+            message="Tx that deployed the Minting script: "
+            type="tx"
+            value={txMintingPolScriptDeployment || ""}
+          />
+          <ExplorerLink
+            message="Stablecoin PolicyId in Hex:"
+            type="policy"
+            value={scPolicyIdHex || ""}
+          />
+          <ExplorerLink
+            message="Stablecoin AssetClass in Hex:"
+            type="asset"
+            value={scAssetClassHex || ""}
+          />
 
-            {/* ACTIONS SECTION */}
-            <div className="flex flex-col items-center gap-8 overflow-scroll h-full py-20 bg-zinc-50 w-4/5 rounded-2xl">
-                <div className="shadow-[0_4px_0px_0px_rgba(0,0,0,0.25)] w-[664px] bg-zinc-50 border border-zinc-600 rounded-xl p-9">
-                    <MintNFTButton />
-                    <Oracle />
-                </div>
+          <div className="font-quicksand h-16 w-full overflow-hidden">
+            <p className=" bg-zinc-800  text-base text-zinc-100 h-8 pt-[6px] pl-2">
+              Depoyed with Minimum Percentage of Collateral:
+            </p>{" "}
+            {minPercent}
+          </div>
+        </div>
+      </div>
 
-                <div className="shadow-[0_4px_0px_0px_rgba(0,0,0,0.25)] w-[664px] bg-zinc-50 border border-zinc-600 rounded-xl px-9 pb-9">
-                    <DeployScripts />
-                </div>
-                <MintStablecoin />
-            </div>
-        </main>
-    );
+      {/* PERSON BUTTONS */}
+      <div className="absolute top-4 left-5 flex flex-row gap-4">
+        <button
+          onClick={() => handleClick("oracle")}
+          className={`${
+            isPerson == "oracle"
+              ? "bg-zinc-100 text-zinc-800 shadow-[0_5px_0px_0px_rgba(255,251,251,0.6)]"
+              : "bg-zinc-900 text-zinc-50 shadow-[0_5px_0px_0px_rgba(0,0,0,0.6)]"
+          } font-quicksand text-lg font-bold py-3 px-8 rounded-lg active:translate-y-[2px] active:shadow-[0_4px_0px_0px_rgba(0,0,0,0.6)] `}
+        >
+          Oracle
+        </button>
+        <button
+          onClick={() => handleClick("owner")}
+          className={`${
+            isPerson == "owner"
+              ? "bg-zinc-100 text-zinc-800 shadow-[0_5px_0px_0px_rgba(255,251,251,0.6)]"
+              : "bg-zinc-900 text-zinc-50 shadow-[0_5px_0px_0px_rgba(0,0,0,0.6)]"
+          }  font-quicksand text-lg font-bold py-3 px-8 rounded-lg active:translate-y-[2px] active:shadow-[0_4px_0px_0px_rgba(0,0,0,0.6)] `}
+        >
+          Owner
+        </button>
+        <button
+          onClick={() => handleClick("user")}
+          className={`${
+            isPerson == "user"
+              ? "bg-zinc-100 text-zinc-800 shadow-[0_5px_0px_0px_rgba(255,251,251,0.6)]"
+              : "bg-zinc-900 text-zinc-50 shadow-[0_5px_0px_0px_rgba(0,0,0,0.6)]"
+          }  font-quicksand text-lg font-bold py-3 px-8 rounded-lg active:translate-y-[2px] active:shadow-[0_4px_0px_0px_rgba(0,0,0,0.6)] `}
+        >
+          User
+        </button>
+      </div>
+
+      {/* ACTIONS SECTION */}
+      <div className="flex flex-col items-center gap-8  h-full py-12 bg-zinc-50 w-4/5 rounded-2xl">
+        {/* ORACLE ACTIONS */}
+        {isPerson == "oracle" && (
+          <div className="shadow-[0_4px_0px_0px_rgba(0,0,0,0.25)] w-[664px] bg-zinc-50 border border-zinc-600 rounded-xl p-9">
+            <MintNFTButton />
+            <Oracle />
+          </div>
+        )}
+
+        {/* OWNER ACTIONS */}
+        {isPerson == "owner" && (
+          <div className="shadow-[0_4px_0px_0px_rgba(0,0,0,0.25)] w-[664px] bg-zinc-50 border border-zinc-600 rounded-xl px-9 pb-9">
+            <DeployScripts />
+          </div>
+        )}
+
+        {/* USER ACTIONS */}
+        {isPerson == "user" && <MintStablecoin />}
+      </div>
+    </main>
+  );
 }
